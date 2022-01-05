de388a3e55431b752d14252f30efc6f92fd974db
Eduardo Bresolin <75597388+BresoDEV@users.noreply.github.com>
2022-01-05 03:46:42 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace RGB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(196, 94);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Iniciar RGB";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Cor :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Lento",
+            "Normal",
+            "Rapido"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(196, 21);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.Text = "Velocidade da troca de cores";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(276, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(70, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Vermelho";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(276, 35);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(54, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Verde";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(276, 59);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(46, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Azul";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(276, 82);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(56, 17);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "Cyano";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(276, 105);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(64, 17);
+            this.checkBox5.TabIndex = 8;
+            this.checkBox5.Text = "Amarelo";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(276, 128);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(51, 17);
+            this.checkBox6.TabIndex = 9;
+            this.checkBox6.Text = "Roxo";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(276, 151);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(51, 17);
+            this.checkBox7.TabIndex = 10;
+            this.checkBox7.Text = "Preto";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(276, 174);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(60, 17);
+            this.checkBox8.TabIndex = 11;
+            this.checkBox8.Text = "Branco";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Cor HEX :";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 219);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Parar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 254);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.MinimumSize = new System.Drawing.Size(237, 243);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RGB";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RGB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public static int r = 0;
+        public static int g = 0;
+        public static int b = 0;
+        public static int fim = 255;
+        public static int inicio = 0;
+        public static bool passo1 = true;
+        public static bool passo2 = false;
+        public static bool passo3 = false;
+        public static bool passo4 = false;
+        public static int vel = 1;//multiplos de 5 somente
+
+
+        public static void rgb(int velocidade)
+        {
+
+            if (passo1)
+            {
+                if (r != 255) { r = r + vel; }
+                if (r == 255 && g != 255) { g = g + vel; }
+                if (r == 255 && g == 255 && b != 255) { b = b + vel; }
+                if (r == 255 && g == 255 && b == 255) { passo1 = false; passo2 = true; }
+            }
+
+            if (passo2)
+            {
+                if (r != 0) { r = r - vel; }
+                if (r == 0 && g != 0) { g = g - vel; }
+                if (r == 0 && g == 0 && b != 0) { b = b - vel; }
+                if (r == 0 && g == 0 && b == 0) { passo2 = false; passo3 = true; }
+            }
+
+            if (passo3)
+            {
+                if (g != 255) { g = g + vel; }
+                if (g == 255) { g = g - vel; passo3 = false; passo4 = true; }
+            }
+            if (passo4)
+            {
+                if (g != 0 && b != 255 && r != 255) { g = g - vel; b = b + vel; r = r + vel; }
+                if (r != 0 && g == 0 && b != 0) { r = r - vel; b = b - vel; }
+                if (r == 0 && g == 0 && b == 0) { passo4 = false; passo3 = false; passo2 = false; passo1 = true; }
+            } 
+        }
+
+
+        private static String HexConverter(int r, int g, int b)
+        {
+            return "#" + r.ToString("X2") + g.ToString("X2") + b.ToString("X2");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (comboBox1.Text == "Lento")
+            {
+                vel = 1;
+                timer1.Enabled = true;
+            }
+            else if (comboBox1.Text == "Normal")
+            {
+                vel = 5;
+                timer1.Enabled = true;
+            }
+            else if (comboBox1.Text == "Rapido")
+            {
+                vel = 15;
+                timer1.Enabled = true;
+            }
+            else if (comboBox1.Text == "Velocidade da troca de cores")
+            {
+                MessageBox.Show("Escolha uma velocidade para as cores");
+            }
+            else
+            {
+                MessageBox.Show("Escolha uma velocidade valida");
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+
+            if (r == 255 && g == 1 && b == 0)//vermelho
+                checkBox1.Checked = true;
+            if (r == 0 && g == 254 && b == 0)//verde
+                checkBox2.Checked = true;
+            if (r == 0 && g == 1 && b == 255)//azul
+                checkBox3.Checked = true;
+            if (r == 1 && g == 255 && b == 255)//cyano
+                checkBox4.Checked = true;
+            if (r == 255 && g == 255 && b == 1)//amarelo
+                checkBox5.Checked = true;
+            if (r == 190 && g == 0 && b == 190)//roxo
+                checkBox6.Checked = true;
+            if (r == 0 && g == 0 && b == 0)//preto
+                checkBox7.Checked = true;
+            if (r == 255 && g == 255 && b == 254)//branco
+                checkBox8.Checked = true;
+
+
+
+            rgb(vel);
+            pictureBox1.BackColor = System.Drawing.Color.FromArgb(255, r, g, b);
+            label1.Text = "Cor atual: " + Convert.ToString(r) + ", " + Convert.ToString(g) + ", " + Convert.ToString(b);
+            label2.Text = "Cor Hex: " + HexConverter(r, g, b);
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+    }
+}
